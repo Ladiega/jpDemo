9a1317bf18f29db92e7d2b34c930f678ad9e2d65
Ladiega <Fokiuatodo@gmail.com>
2021-10-12 15:28:54 -0500

    iphone beta (footer and nav)

--- pages/iphone.js
+++ pages/iphone.js
@@ -2,27 +2,41 @@ import styles from '../styles/iphone.module.css'
 import Image from 'next/image'
 import jpimg from '../public/img/jpbground.png'
 import Nav from '../componets/navbar/index'
+import Footer from '../componets/footer/index'
 
 export default function Iphone(){
     return(
         <div className={styles.global_container}>
             
             <div className={styles.background_1}>
-            
                 <Image
                     className={styles.buritica_img}
-                    src={jpimg}
+                    src={jpimg} 
                 />
             </div>
-              
-    
+                        
             <div className={styles.background_2}> </div>
                       
-            <div className={styles.background_3}></div>
+            <div className={styles.background_4}></div>
                
-            <div className={styles.background_4}></div>  
+            <div className={styles.background_3}></div>  
 
             <div className={styles.background_5}></div>
+
+            <div className={styles.background_6}>
+            <Nav 
+            className={styles.nav}
+            />              
+            </div>
+            
+            <div className={styles.background_7}>
+                <Footer
+                className={styles.footer}
+                />
+            </div>
+
+            
+
         </div>
     )
 

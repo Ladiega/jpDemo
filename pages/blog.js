e33872de7b1a2a8503103953270e5f44a9bc3c0a
Ladiega <Fokiuatodo@gmail.com>
2021-10-12 12:55:25 -0500

    navbar and footer v1.0

--- pages/blog.js
+++ pages/blog.js
@@ -1,70 +1,21 @@
 import styles from '../styles/blog.module.css'
-import Image from 'next/image'
-import Link from 'next/link'
+
 import Nav from '../componets/navbar/index'
 import Footer from '../componets/footer/index'
 
-import jplogo from '../public/img/jpicon.svg'
-import instalogo from '../public/img/instaicon.svg'
-import twilogo from '../public/img/twicon.svg'
-import hmenulogo from '../public/img/hmenu.svg'
+
 
 export default function Blog(){
     return(
-        <div className={styles.global_container}>
-            <nav className={styles.nav_container}>
-                <div className={styles.jplogo_container} >
-                   <Link href="/">
-                   <a >
-                    <Image
-                    className={styles.jplogo}
-                    src={jplogo}
-                    width={24}
-                    height={20}
-                    />
-                    </a>
-                    </Link>
-                    </div>
-                    <div className={styles.instalogo_container}>
-                    <Link href="https://www.instagram.com/buritica/?hl=es">
-                    <a >
-                    <Image
-                    className={styles.instalogo}
-                    src={instalogo}
-                    width={18}
-                    height={18}
-                    />
-                    </a>
-                    </Link>
-                    </div>
-                    
-                    <div className={styles.twilogo_container}>
-                   <Link href="https://twitter.com/buritica?lang=es">
-                   <a >
-                    <Image
-                    className={styles.twilogo}
-                    src={twilogo}
-                    width={20}
-                    height={20}
-                    />
-                    </a>
-                    </Link>
-                    </div>
-                    <div className={styles.hmenulogo_container}>
-                    <button type="button" className={styles.hmenu_button}>
-                    <Image
-                    className={styles.hmenulogo}
-                    src={hmenulogo}
-                    width={23}
-                    height={17}
-                    />
-                    </button>
-                    </div>
-                    
-
-            </nav>
-            <Nav/>
-            <Footer/>
+       <div>
+            <div className={styles.blog_container}>
+                <Nav
+                className={styles.nav_container}            
+                />
+                <Footer
+                className={styles.footer_container}
+                />
+            </div>
         </div>
     )
 }
--- pages/iphone.js
+++ pages/iphone.js
@@ -6,18 +6,16 @@ import Nav from '../componets/navbar/index'
 export default function Iphone(){
     return(
         <div className={styles.global_container}>
-            <div className={styles.background_1}><Nav></Nav>
+            
+            <div className={styles.background_1}>
+            
                 <Image
                     className={styles.buritica_img}
                     src={jpimg}
                 />
-               
-
             </div>
-            
-             
-            
-            
+              
+    
             <div className={styles.background_2}> </div>
                       
             <div className={styles.background_3}></div>

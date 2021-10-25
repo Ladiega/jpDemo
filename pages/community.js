53694a82dca4d6dafb90be4b470312cc6c311e18
Ladiega <Fokiuatodo@gmail.com>
2021-10-24 20:10:33 -0500

    pages not empty

---
+++ pages/community.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function Comm (){
+    return(
+        <div>
+            <Link href="/">
+            <a>Community</a>
+            </Link>
+            
+        </div>
+    )
+}
---
+++ pages/photo.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+export default function Photo (){
+    return(
+        <div>
+            <Link href="/">
+            <a >Photo</a>
+            </Link>
+        </div>
+    )
+    
+    
+}
